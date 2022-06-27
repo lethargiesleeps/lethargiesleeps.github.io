b7f06c3d7d05dbe17043356b7e6351c7450fd6f1
Michaël Landry <74638031+lethargiesleeps@users.noreply.github.com>
2022-06-27 00:07:12 -0400

    Comming soon commit

--- src/js/home.js
+++ src/js/home.js
@@ -1,12 +1,12 @@
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(
-    75,
+    300,
     window.innerWidth / window.innerHeight,
     0.1,
     1000
 );
-camera.position.z = 5;
-
+camera.position.z = 50;
+scene.background = new THREE.Color(0x283655);
 var renderer = new THREE.WebGLRenderer({
     alpha: true
 });
@@ -16,14 +16,32 @@ var directionalLight = new THREE.DirectionalLight(0xffffff, 1);
 directionalLight.position.set(0, 2, 2);
 scene.add(directionalLight);
 
+let tubes = Math.floor(Math.random() * 201);
+let x = Math.floor(Math.random() * 2);
+let moveRight = x >= 0.5 ? true : false;
 
-var geometry = new THREE.BoxGeometry(1,1,1);
-var material = new THREE.MeshBasicMaterial({color: '#285cd0'});
-var box = new THREE.Mesh(geometry, material);
-scene.add(box);
+var geometry = new THREE.TorusKnotGeometry(8, 3, tubes, 15, 18, 15);
+var material = new THREE.MeshToonMaterial({color: '#4d648d'});
+var knot = new THREE.Mesh(geometry, material);
+scene.add(knot);
+
+console.log(moveRight, x);
 
 function render() {
-    box.rotation.y += 0.01;
+    if(moveRight) {
+        knot.rotation.z += 0.005;
+        knot.rotation.y -= 0.005;
+    }
+    else {
+        knot.rotation.z -= 0.005;
+        knot.rotation.y += 0.005;
+    }
+    
+    
+    
+    
+
+    
     requestAnimationFrame(render);
     renderer.render(scene, camera);
 }
