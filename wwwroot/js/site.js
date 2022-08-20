3e6757287ea9bbb909bbba03b7998fc5190f868e
Michaël Landry <74638031+lethargiesleeps@users.noreply.github.com>
2022-08-19 20:29:11 -0400

    First projects, some mobile responsiveness

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,7 +1,12 @@
 document.getElementById("subMenu").style.visibility = 'hidden';
-
+document.getElementById("subMenuMobile").style.visibility = 'hidden';
 function openSub() {
 
     let menu = document.getElementById("subMenu").style.visibility;
     document.getElementById("subMenu").style.visibility = (menu === 'visible') ? 'hidden' : 'visible';
+}
+
+function openSubMobile() {
+    let menu = document.getElementById("subMenuMobile").style.visibility;
+    document.getElementById("subMenuMobile").style.visibility = (menu === 'visible') ? 'hidden' : 'visible';
 }
