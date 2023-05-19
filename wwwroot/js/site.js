4f8d2011a000481bf4868e07402413b12d65e1d1
Michaël Landry <74638031+lethargiesleeps@users.noreply.github.com>
2023-05-19 17:34:37 -0400

    overhaul

---
+++ wwwroot/js/site.js
@@ -0,0 +1,159 @@
+const links = document.querySelectorAll('li');
+const borderContainer = document.getElementById('container');
+
+//PROJECTS ELEMENTS//
+const panels = document.querySelectorAll('.panel');
+
+const elements = [
+    {name: 'credentials', value: document.getElementById('credentialsContainer') },
+    {name: 'blurb', value: document.getElementById('homeBlurb') },
+    {name: 'projects', value: document.getElementById('projectCatContainer') },
+    {name: 'webProjects', value: document.getElementById('webProjectsContainer') }
+];
+
+const navIcons = [
+    {name: 'webProjectsToProjects', value: document.getElementById('webProjectsToProjects')}
+];
+
+//PANEL NAV
+navIcons.forEach(n => {
+    n.value.addEventListener('click', () => {
+        switch(n.name) {
+            case 'webProjectsToProjects':
+                goToProjects();
+                break;
+        }
+    });
+})
+//INIT
+links[0].innerText = '☺';
+clearContainer();
+makeVisible(elements[0], 'block');
+makeVisible(elements[1], 'block');
+
+//PANEL INTERACTION
+panels.forEach(panel => {
+    panel.addEventListener('mouseover', () => {
+        removeActiveClasses();
+        panel.classList.add('active');
+    });
+
+    panel.addEventListener('mouseout', removeActiveClasses);
+
+    panel.addEventListener('click', () => {
+        if(panel.classList.contains('active')) {
+            switch(panel) {
+                case panels[0]:
+                    clearContainer();
+                    makeVisible(elements[3], 'flex');
+                    break;
+                case panels[1]:
+                    console.log('Mobile Projects');
+                    break;
+                case panels[2]:
+                    console.log('Desktop Projects');
+                    break;
+                case panels[3]:
+                    //window.open('https://lethargiesleeps.github.io/FrontEndProjects', '_blank');
+                    break;
+                default:
+                    console.log('404: Panel Not Found');
+            }
+        }
+    })
+
+})
+
+//MAIN NAV INTERACTION
+for(let i = 0; i < links.length; i++) {
+    links[i].addEventListener("mouseover", () => {
+        links[i].classList.add('on-hover-nav');
+    });
+
+    links[i].addEventListener("mouseout", () => {
+        links[i].classList.remove('on-hover-nav');
+    });
+
+    links[i].addEventListener("click", () => {
+        switch(i) {
+            case 0:
+                pageTitle.innerText = "Michaël Landry - Portfolio";
+                goToHome();
+                break;
+            case 1:
+                pageTitle.innerText = "Michaël Landry - Projects";
+                goToProjects();
+                break;
+            case 2:
+                pageTitle.innerText = "Michaël Landry - Resume";
+                goToResume();
+                break;
+            case 3:
+                pageTitle.innerText = "Michaël Landry - Connect";
+                goToConnect();
+                break;
+            default:
+                pageTitle.innerText = "Michaël Landry - 404";
+        }
+        
+        
+    });
+
+}
+
+function goToHome() {
+    resetNavTitles();
+    links[0].innerText = '☺'
+    clearContainer();
+    makeVisible(elements[0], 'block');
+    makeVisible(elements[1], 'block');
+    
+}
+
+function goToProjects() {
+    resetNavTitles();
+    links[1].innerText = '☺'
+    clearContainer();
+    makeVisible(elements[2], 'flex');
+    
+}
+
+function goToResume() {
+    resetNavTitles();
+    links[2].innerText = '☺'
+    clearContainer();
+}
+
+function goToConnect() {
+    resetNavTitles();
+    links[3].innerText = '☺'
+    clearContainer();
+}
+
+
+//INTERNAL FUNCTIONS//
+function removeActiveClasses() {
+    panels.forEach(panel => {
+        panel.classList.remove('active');
+    })
+}
+
+function resetNavTitles() {
+    links[0].innerText = 'Home';
+    links[1].innerText = 'Projects';
+    links[2].innerText = 'Resume';
+    links[3].innerText = 'Connect';
+}
+
+function clearContainer() {
+    elements.forEach(e => {
+        e.value.style.display = 'none';
+    })
+}
+
+function makeVisible(object, type) {
+    object.value.style.display = type;
+}
+
+
+
